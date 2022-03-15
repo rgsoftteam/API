320eb2656d053846edb0e1583cb4ba40a63ec76a
RGsoftteam <rgsoftteam@gmail.com>
2022-03-15 20:43:28 +0300

    final_ver

--- python_repos.py
+++ python_repos.py
@@ -14,17 +14,16 @@ print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# анализ первого репозитория
-repo_dict = repo_dicts[0]
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
 
-print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Reposirory: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+	print(f"\nName: {repo_dict['name']}")
+	print(f"Owner: {repo_dict['owner']['login']}")
+	print(f"Stars: {repo_dict['stargazers_count']}")
+	print(f"Reposirory: {repo_dict['html_url']}")
+	print(f"Created: {repo_dict['created_at']}")
+	print(f"Updated: {repo_dict['updated_at']}")
+	print(f"Description: {repo_dict['description']}")
 
 print(f"\nKeys: {len(repo_dict)}")
 for key in sorted(repo_dict.keys()):
